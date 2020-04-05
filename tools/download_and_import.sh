00637f658e32e90f4937f4d2d5b56e80f69aee8f
Bráulio Bezerra <brauliobezerra@gmail.com>
2020-04-05 13:16:59 -0300

    Cleanup temp files

--- tools/download_and_import.sh
+++ tools/download_and_import.sh
@@ -2,12 +2,18 @@
 
 set -x
 
-# wget -c https://downloads.tatoeba.org/exports/sentences.tar.bz2
-# wget -c https://downloads.tatoeba.org/exports/links.tar.bz2 
+wget -c https://downloads.tatoeba.org/exports/sentences.tar.bz2
+wget -c https://downloads.tatoeba.org/exports/links.tar.bz2 
 
-# tar xf sentences.tar.bz2
-# tar xf links.tar.bz2
+tar xf sentences.tar.bz2
+rm     sentences.tar.bz2
+tar xf links.tar.bz2
+rm     links.tar.bz2
 
-# mv type.braul.io.sqlite3 type.braul.io.sqlite3.old -f
 sed 's/"/\\"/g' sentences.csv > sentences.fixed.csv
+rm sentences.csv
+
 sqlite3 -echo type.braul.io.sqlite3 < sqlite3_cmds.sql
+rm sentences.fixed.csv
+rm links.csv
+
